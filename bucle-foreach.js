4b297ee763098d22e903c765ea996c7b9c1b254f
AElenaGr <141139917+AElenaGr@users.noreply.github.com>
2023-11-03 10:02:02 +0100

    Add files via upload

---
+++ bucle-foreach.js
@@ -0,0 +1,10 @@
+const animales = ['tommy','gato','panson'];
+
+numbers.forEach(function(animal, index) {
+    console.log(`Elemento en el índice ${index}: ${animal}`);
+});
+
+
+animales.forEach((animal,index)=>(
+  console.log(animal);
+  ));
